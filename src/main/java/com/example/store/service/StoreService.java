af747fa4400878d130e1e77f22a1391eda2f82da
NaughtyCoon <sterrestrian@gmail.com>
2025-08-31 20:30:40 +0300

    3

--- src/main/java/com/example/store/service/StoreService.java
+++ src/main/java/com/example/store/service/StoreService.java
@@ -2,9 +2,11 @@ package com.example.store.service;
 
 import com.example.store.dto.StoreResponseDto;
 import com.example.store.entity.Store;
+import com.example.store.mapper.StoreMapper;
 import com.example.store.repository.StoreRepository;
 import com.example.store.request.StoreRequest;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,13 +19,43 @@ public class StoreService {
     @Autowired
     private StoreRepository storeRepository;
 
+    @Autowired
+    private StoreMapper storeMapper;
+
     public StoreResponseDto createStore(StoreRequest request){
 
         Store store = new Store(UUID.randomUUID(), request.getName(), request.getLocation(), null);
 
         storeRepository.saveAndFlush(store);
 
-        return new StoreResponseDto(store.getId(), store.getName(), store.getLocation());
+        return storeMapper.mapToStoreResponseDto(store);
+
+    }
+
+    public void deleteStore(UUID storeId){
+
+        storeRepository.deleteById(storeId);
+
+    }
+
+    public StoreResponseDto findById(UUID storeId){
+
+        Store store = storeRepository.findById(storeId).orElseThrow();
+
+        return storeMapper.mapToStoreResponseDto(store);
+
+    }
+
+    public StoreResponseDto updateById(UUID id, @Valid StoreRequest request) {
+
+        Store store = storeRepository.findById(id).orElseThrow();
+
+        store.setName(request.getName());
+        store.setLocation(request.getLocation());
+
+        storeRepository.saveAndFlush(store);
+
+        return storeMapper.mapToStoreResponseDto(store);
 
     }
 
